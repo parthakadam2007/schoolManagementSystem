da3166c9168e7ad250a847d2c2330c0486df697c
partha kadam <parthakadam2007@gmail.com>
2025-07-06 21:31:23 +0530

    home route

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from "react-router";
 
 const Home = () => {
     return (
         <div>
+                 <Link to="/StudentLogin">Student Login</Link>;
+                  <Link to="/TeacherLogin">Teacher Login</Link>;
             <h1>Welcome to the School System</h1>
         </div>
     );
